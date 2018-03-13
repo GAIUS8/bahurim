48b33381b0ba49e0ed35bd8750ed99af9aca3a1e
GAIUS8 <gaius827@gmail.com>
2018-03-13 13:08:45 +0900

    add nav

--- bhrserver/settings.py
+++ bhrserver/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(ROOT_DIR, 'static'),
 ]
 
 # log
